398dee2e44a5136a100c3878b7bc973c6f704ac6
Luc Sinet <luc.sinet@etixgroup.com>
2017-07-05 23:19:26 +0200

    Add a specialisation to have an audio manager, play a sound on startup.

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Window/Event.hpp>
@@ -18,6 +20,12 @@ class Window {
     _textureManager.load("main", "asset.png");
     const auto& texture = _textureManager.get("main");
     auto nbTiles = _tileManager.parseTexture(texture, 32);
+
+    _audioManager.load("coin", "coin.wav");
+    const auto& soundBuffer = _audioManager.get("coin");
+    sf::Sound coinSound(soundBuffer);
+
+    coinSound.play();
 
     while (_window.isOpen()) {
       while (_window.pollEvent(event)) {
@@ -42,6 +50,7 @@ class Window {
  private:
   sf::RenderWindow _window;
   Ressources::Manager<sf::Texture> _textureManager;
+  Ressources::Manager<sf::SoundBuffer> _audioManager;
   Ressources::TileManager _tileManager;
 };
 
